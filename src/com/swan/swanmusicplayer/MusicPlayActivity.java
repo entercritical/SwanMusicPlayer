a464de1dbc941ee117c5af2c3e204b27de085a17
Suhwan Hwang <entercritical@gmail.com>
2013-10-14 00:21:07 +0900

    music play

--- src/com/swan/swanmusicplayer/MusicPlayActivity.java
+++ src/com/swan/swanmusicplayer/MusicPlayActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -17,6 +19,7 @@ public class MusicPlayActivity extends Activity {
     private MusicListAdapter mMusicListAdapter;
     private Music mMusic;
     private int mPosition;
+    private boolean mIsPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,8 @@ public class MusicPlayActivity extends Activity {
         // initialize views
         initViews();
         
+        // play music
+        play();
     }
     
     /**
@@ -46,10 +51,32 @@ public class MusicPlayActivity extends Activity {
      */
     private void initViews() {
         setContentView(R.layout.music_player);
+        
         mAlbumImage = (ImageView)findViewById(R.id.albumImage);
         mPrev = (ImageButton)findViewById(R.id.prevButton);
+        mPrev.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                prev();
+            }
+        });
         mNext = (ImageButton)findViewById(R.id.nextButton);
+        mNext.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                next();
+            }
+        });
         mPlay = (ImageButton)findViewById(R.id.playButton);
+        mPlay.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                playToggle();
+            }
+        });
 
         if (mMusic != null) {
             // Set album art image
@@ -63,6 +90,107 @@ public class MusicPlayActivity extends Activity {
             // Set Title
             setTitle(mMusic.getTitle());
         }
+    }
+    
+    /**
+     * play music
+     */
+    private void play() {
+        Intent intent = new Intent(getBaseContext(), MusicPlayService.class);
+        intent.setAction(MusicPlayService.ACTION_PLAY);
+        intent.putExtra("position", mPosition);
+        startService(intent);
+        
+        mIsPlaying = true;
+        mPlay.setImageResource(R.drawable.av_pause);    // change play button image
+    }
+    
+    /**
+     * pause music
+     */
+    private void pause() {
+        Intent intent = new Intent(getBaseContext(), MusicPlayService.class);
+        intent.setAction(MusicPlayService.ACTION_PAUSE);
+        intent.putExtra("position", mPosition);
+        startService(intent);
+        
+        mIsPlaying = false;
+        mPlay.setImageResource(R.drawable.av_play);     // change play button image
+    }
+    
+    /**
+     * play or pause by playing status
+     */
+    private void playToggle() {
+        Intent intent = new Intent(getBaseContext(), MusicPlayService.class);
+        intent.setAction(MusicPlayService.ACTION_PLAY_TOGGLE);
+        intent.putExtra("position", mPosition);
+        startService(intent);
+        
+        mPlay.setImageResource(mIsPlaying ? R.drawable.av_play : R.drawable.av_pause);    // change play button image
+        mIsPlaying = !mIsPlaying;
+    }
+    
+    /**
+     * play previous music
+     */
+    private void prev() {
+        Intent intent = new Intent(getBaseContext(), MusicPlayService.class);
+        intent.setAction(MusicPlayService.ACTION_PREV);
+        startService(intent);
+        
+        if (mPosition > 0) {
+            mPosition--;
+        } else {
+            mPosition = mMusicListAdapter.getCount();
+        }
+        mMusic = (Music)mMusicListAdapter.getItem(mPosition);
+        if (mMusic != null) {
+            // Set album art image
+            Bitmap albumArt = mMusic.getAlbumArt(this);
+            if (albumArt != null) {
+                mAlbumImage.setImageBitmap(albumArt);
+            } else {
+                mAlbumImage.setImageResource(R.drawable.music);
+            }
+            
+            // Set Title
+            setTitle(mMusic.getTitle());
+        }       
+
+        mIsPlaying = true;
+        mPlay.setImageResource(R.drawable.av_pause);    // change play button image
+    }
+    
+    /**
+     * play next music
+     */
+    private void next() {
+        Intent intent = new Intent(getBaseContext(), MusicPlayService.class);
+        intent.setAction(MusicPlayService.ACTION_NEXT);
+        startService(intent);
+        
+        if (mPosition < mMusicListAdapter.getCount() - 1) {
+            mPosition++;
+        } else {
+            mPosition = 0;
+        }
+        mMusic = (Music)mMusicListAdapter.getItem(mPosition);
+        if (mMusic != null) {
+            // Set album art image
+            Bitmap albumArt = mMusic.getAlbumArt(this);
+            if (albumArt != null) {
+                mAlbumImage.setImageBitmap(albumArt);
+            } else {
+                mAlbumImage.setImageResource(R.drawable.music);
+            }
+            
+            // Set Title
+            setTitle(mMusic.getTitle());
+        } 
+        
+        mIsPlaying = true;
+        mPlay.setImageResource(R.drawable.av_pause);    // change play button image
     }
 
     @Override
--- src/com/swan/swanmusicplayer/MusicPlayService.java
+++ src/com/swan/swanmusicplayer/MusicPlayService.java
@@ -1,23 +1,33 @@
 package com.swan.swanmusicplayer;
 
+import java.io.IOException;
+
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
 
-public class MusicPlayService extends Service implements OnErrorListener {
-    private static final String TAG = "PlaybackService";
-    
-    public static final String ACTION_PLAY = "com.swan.action.PLAY";
-    public static final String ACTION_PAUSE = "com.swan.action.PAUSE";
-    public static final String ACTION_NEXT_SONG = "com.swan.action.NEXT_SONG";
-    public static final String ACTION_PREV_SONG = "com.swan.action.PREV_SONG";
-    
+public class MusicPlayService extends Service implements OnErrorListener, OnCompletionListener {
+    private static final String TAG = "MusicPlayService";
+
+    public static final String ACTION_PREV = "com.swan.swanmusicplayer.action.PREV";
+    public static final String ACTION_NEXT = "com.swan.swanmusicplayer.action.NEXT";
+    public static final String ACTION_PLAY = "com.swan.swanmusicplayer.action.PLAY";
+    public static final String ACTION_PAUSE = "com.swan.swanmusicplayer.action.PAUSE";
+    public static final String ACTION_PLAY_TOGGLE = "com.swan.swanmusicplayer.action.PLAY_TOGGLE";
+
     private MediaPlayer mPlayer;
+    private MusicListAdapter mMusicListAdapter;
+    private int mCurrentPosition;
+    private MusicPlayReceiver mMusicPlayReceiver;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -28,33 +38,58 @@ public class MusicPlayService extends Service implements OnErrorListener {
     public void onCreate() {
         Log.d(TAG, "onCreate()");
         super.onCreate();
-        
-//        Uri myUri = ....; // initialize Uri here
-//        MediaPlayer mediaPlayer = new MediaPlayer();
-//        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//        mediaPlayer.setDataSource(getApplicationContext(), myUri);
-//        mediaPlayer.prepare();
-//        mediaPlayer.start();
-        
+
+        // Uri myUri = ....; // initialize Uri here
+        // MediaPlayer mediaPlayer = new MediaPlayer();
+        // mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        // mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        // mediaPlayer.prepare();
+        // mediaPlayer.start();
+
         mPlayer = new MediaPlayer();
         mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mPlayer.setOnErrorListener(this);
+        mPlayer.setOnCompletionListener(this);
+
+        // Broadcast Receiver
+        mMusicPlayReceiver = new MusicPlayReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
+        registerReceiver(mMusicPlayReceiver, filter);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
+            mMusicListAdapter = MusicListActivity.getMusicListAdapter();
+
             String action = intent.getAction();
             Log.d(TAG, "onStartCommand() " + action);
 
-            if (ACTION_PLAY.equals(action)) {
+            if (ACTION_PREV.equals(action)) {
+                if (mCurrentPosition > 0) {
+                    mCurrentPosition--;
+                } else {
+                    mCurrentPosition = mMusicListAdapter.getCount() - 1;                    
+                }
+                play(mCurrentPosition);
+            } else if (ACTION_NEXT.equals(action)) {
+                if (mCurrentPosition < mMusicListAdapter.getCount() - 1) {
+                    mCurrentPosition++;
+                } else {
+                    mCurrentPosition = 0;                    
+                }
+                play(mCurrentPosition);
+            } else if (ACTION_PLAY.equals(action)) {
+                int position = intent.getIntExtra("position", -1);
                 
+                if (position != -1) {
+                    play(position);
+                }
             } else if (ACTION_PAUSE.equals(action)) {
-                
-            } else if (ACTION_NEXT_SONG.equals(action)) {
-                
-            } else if (ACTION_PREV_SONG.equals(action)) {
-                
+                pause();
+            } else if (ACTION_PLAY_TOGGLE.equals(action)) {
+                playToggle();
             }
         }
         return START_NOT_STICKY;
@@ -64,19 +99,97 @@ public class MusicPlayService extends Service implements OnErrorListener {
     public void onDestroy() {
         Log.d(TAG, "onDestroy()");
         super.onDestroy();
+
+        mMusicListAdapter = null; // for GC
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(int position) {
+        mCurrentPosition = position;
+
+        if (mMusicListAdapter == null) {
+            Log.d(TAG, "play(): MediaPlayer is null");
+            return;
+        }
+        
+        Music music = (Music) mMusicListAdapter.getItem(mCurrentPosition);
+        if (music == null) {
+            Log.d(TAG, "play() : No music");
+            return;
+        }
+        
+        if (mPlayer == null) {
+            Log.d(TAG, "play(): MediaPlayer is null");
+            return;
+        }
+        
+        try {
+            mPlayer.reset();
+            mPlayer.setDataSource(music.getData());
+            mPlayer.prepare();
+            mPlayer.seekTo(0);
+            mPlayer.start();
+        } catch (Exception e) {
+            Log.d(TAG, "play error");
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        if (mPlayer == null) {
+            Log.d(TAG, "pause(): MediaPlayer is null");
+            return;
+        }
+        mPlayer.pause();
     }
     
-    public int play() {
-        return 0;
-    }
-    
-    public int pause() {
-        return 0;
+    public void playToggle() {
+        if (mPlayer == null) {
+            Log.d(TAG, "playToggle(): MediaPlayer is null");
+            return;
+        }
+        
+        if (mPlayer.isPlaying()) {
+            mPlayer.pause();
+        } else {
+            mPlayer.start();
+        }
     }
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    /**
+     * Broadcast Receiver for HEADSET
+     * @author Suhwan Hwang
+     *
+     */
+    private class MusicPlayReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            
+            // Headset unplugged 
+            if (Intent.ACTION_HEADSET_PLUG.equals(action)) {
+                if (intent.getIntExtra("state", 0) == 0
+                        && mPlayer.isPlaying()) {
+                    mPlayer.stop();
+                }
+            }
+        }
+        
     }
 }
